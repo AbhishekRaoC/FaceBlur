4a5fba14b8cf1b07eed913d74455f348670305ab
AbhishekRaoC <abhishekraochimbili@gmail.com>
2019-10-25 19:31:09 +0400

    Deleted everything to restart

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
-import dlib
-import cv2
 
-stream = cv2.VideoCapture(0)
+"""
+-Takes in a video clip
+-In each scene it looks for faces
+-Using the coordinates it blurs the face region
+-Then searches the next scene
+-Finally stitches all images into one video
+"""
+import os
 
-face = dlib.get_frontal_face_detector()
+def breakVideo(video):
+    audioCommand = ("ffmpeg -i ", video, "videoSound.mp3")
+    imageCommand = ("ffmpeg -i ", video, "images%06d.jpg -hide_banner")
+    os.system(audioCommand)
+    os.system(imageCommand)
+    return(True)
 
-def blurOut(face):
-    # code to blur out faces
+def findFace():
+    faceCoordinates = 1
+    return(faceCoordinates)
 
-    return True
+def blurCoordinates():
+    liveImage = 1
+    return(liveImage)
 
-i = 0
-
-while True:
-    while((i % 3) != 0):
-        (grabbed, frame) = stream.read()
-        
+def stitchImages(frameRate, resolution):
+    stitchCommand = ("ffmpeg -r ", frameRate, "-s ", resolution, "-i images%04d.png -i videoSound.mp3 -vcodec libx264 -b 4M -vpre normal -acodec copy FinalVideo.mp4 ")
+    os.system(imageCommand)
